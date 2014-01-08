ea3acd0fa9575655230b7ed5c79d0fa9ff39cbdc
Sergey Chikuyonok <serge.che@gmail.com>
2014-01-08 17:38:49 +0400

    Dependency graph builder

---
+++ lsutils/depgraph.py
@@ -0,0 +1,127 @@
+# Dependency graph builder:
+# creates dependency graph for given LESS or SCSS source
+# and provides a list of dependencies when diffing and patching
+# sources 
+import time
+import zlib
+import depreader
+import os.path
+
+__cache = {}
+__log = False
+
+def crc(file_path):
+	content = open(file_path,"rb").read()
+	return "%X" % (zlib.crc32(content) & 0xFFFFFFFF)
+
+class Dependency():
+	def __init__(self, url):
+		self.url = url
+		self.crc = None
+		self._content = None
+		self.check_time = 0
+		self.update(time.time())
+
+	def update(self, check_time):
+		log = globals()['__log']
+		if self.check_time >= check_time:
+			# item was already checked and contains most recent data
+			if log: print('time guard for %s' % self.url)
+			return False
+
+		self.check_time = time.time()
+		file_crc = crc(self.url)
+
+		if file_crc == self.crc:
+			# file content did't changed since last check
+			if log: print('crc guard for %s' % self.url)
+			self.validate()
+			return False
+
+		self.crc = file_crc
+		self.deps = depreader.find_dependencies(self.url, self.content())
+		return True
+
+	def content(self):
+		if self._content is None:
+			self._content = depreader.read(self.url)
+
+		return self._content
+
+	def validate(self):
+		"Validates dependencies: make sure they exists"
+		if not self.deps:
+			return
+
+		deps = []
+		for d in self.deps:
+			if os.path.isfile(d):
+				deps.append(d)
+
+		self.deps = deps
+
+	def release_content(self):
+		self._content = None
+		
+
+def dependencies(url, content):
+	"Returns plain list of dependencies for given URL and content."
+	local_cache = set()
+	# in case of circular references, forbid parsing current url
+	local_cache.add(url)
+	t = time.time()
+	result = []
+	deps = depreader.find_dependencies(url, content)
+	for d in deps:
+		result += resolve_deps(d, t, local_cache)
+
+	return result
+
+	
+def resolve_deps(url, check_time=None, local_cache=None):
+	if check_time is None:
+		check_time = time.time()
+
+	if local_cache is None:
+		local_cache = set()
+
+	if url in local_cache:
+		# item already in cache, no need to parse it
+		return []
+
+	local_cache.add(url)
+
+	if url not in __cache:
+		__cache[url] = Dependency(url)
+	else:
+		__cache[url].update(check_time)
+
+	result = [__cache[url]]
+	for d in __cache[url].deps:
+		result += resolve_deps(d, check_time, local_cache)
+
+	return result
+
+def free_mem(time_delta):
+	"Removes old entries from cache, free'ing up some memory"
+	t = time.time() - time_delta;
+
+	for k, v in __cache.items():
+		if v.check_time < t:
+			del __cache[k]
+
+if __name__ == '__main__':
+	import sys
+	url = sys.argv[-1]
+	globals()['__log'] = True
+
+	print('Parsing dependencied for %s' % url)
+	content = depreader.read(url)
+	print([d.url for d in dependencies(url, content)])
+
+	# test cache
+	print([d.url for d in dependencies(url, content)])
+
+	free_mem(-1)
+	print('Cached items: %d' % len(__cache))
+
---
+++ lsutils/depreader.py
@@ -0,0 +1,132 @@
+import re
+import codecs
+import os.path
+
+re_import = re.compile(r'@import\s+(?:\((?:less|css)\)\s*)?(url\(.+?\)|\'.+?\'|".+?")')
+
+def find_dependencies(file_path, content=None):
+	"Finds @import dependencies in given file"
+	out = []
+	if content is None:
+		content = read(file_path)
+	content = strip_comments(content)
+	base = os.path.dirname(file_path)
+	for m in re_import.finditer(content):
+		url = m.group(1)
+		if url.startswith('url('):
+			url = url[4:-1]
+		if url[0] in '\'"':
+			url = url[1:-1]
+
+		url = resolve_url(url, base)
+		if url:
+			out.append(url)
+
+	return out
+
+def read(file_path):
+	with codecs.open(file_path, 'r', 'utf-8') as f:
+		return f.read()
+
+
+def strip_comments(str):
+	"Strips comments from given string"
+	ranges = []
+	stream = enumerate(str)
+
+	def peek(pos):
+		if len(str) > pos + 1:
+			return str[pos + 1]
+		return ''
+
+	for i, ch in stream:
+		if ch == '"' or ch == "'":
+			skip_string(stream, ch)
+			continue
+
+		if ch == '/':
+			ch2 = peek(i)
+			if ch2 == '*':
+				# multiline CSS comment
+				start = i
+				ix = str.find('*/', i)
+				if ix == -1:
+					ix = len(str) - 1
+				else:
+					ix += 2
+
+				# move iterator to given index
+				for i, ch in stream:
+					if i == ix:
+						break
+
+				ranges.append((start, ix))
+			elif ch2 == '/':
+				# preprocessor's single line comment
+				start = i
+				for ix, ch in stream:
+					if ch == '\n' or ch == '\r':
+						break
+
+				ranges.append((start, ix))
+
+	return replace_with(str, ranges, ' ')
+
+def skip_string(stream, quote):
+	for i, ch in stream:
+		if ch == '\\':
+			continue
+		if ch == quote:
+			return True
+
+	return False
+
+def replace_with(str, ranges, ch):
+	"""
+	Generic method to replace substrings in given string.
+	In Python 2, `unicode` objects are immutable so we can't
+	just use str[a:b] = new_substring
+	"""
+	if not ranges:
+		return str
+
+	offset = 0
+	out = u''
+	fragments = []
+	for start, end in ranges:
+		out += str[offset:start] + ch * (end - start)
+		offset = end
+
+	out += str[offset:]
+
+	return out
+
+def resolve_url(url, base):
+	files = [url]
+	if '.' not in url:
+		files.append(u'%s.less' % url)
+
+	if not os.path.isabs(url):
+		# not an absolute path, resolve as relative
+		for f in files:
+			p = os.path.abspath(os.path.join(base, f))
+			if os.path.isfile(p):
+				return p
+		
+		# path not found: broken dependency
+		return None
+
+	# resolve absolute path intelligently
+	files = [f[1:] for f in files]
+	parent = base
+	prev_parent = None
+	while parent and os.path.isdir(parent) and parent != prev_parent:
+		for f in files:
+			p = os.path.abspath(os.path.join(parent, f))
+			if os.path.isfile(p):
+				return p
+
+		prev_parent = parent
+		parent = os.path.dirname(parent)
+
+	return None
